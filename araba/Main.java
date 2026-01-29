fb5cb683331309ab56e698d649fde2a5ec3238c8
selahattin kanjo <s.kanjjo@gmail.com>
2026-01-29 05:17:15 +0300

    Add files via upload

---
+++ araba/Main.java
@@ -0,0 +1,11 @@
+
+ public class Main {
+    public static void main(String[] args) {
+        Car c1 =new Car();
+        c1.model=2022;
+        c1.setName("BMW");
+        System.out.println("Car Name: " + c1.getName());
+
+            
+    }
+}
